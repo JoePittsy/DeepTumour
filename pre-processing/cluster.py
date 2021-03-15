6d06711217d94d1a6798b0b39866a9c28032791d
Joe Pitts <joepittsy@gmail.com>
2021-03-15 11:06:22 +0000

    Cleanup

--- pre-processing/cluster.py
+++ pre-processing/cluster.py
@@ -1,6 +1,8 @@
 import pylidc as pl
 from vectors import Vector4D
 from kmeans import *
+import pprint
+from scipy.stats import pearsonr
 
 
 def get_data():
@@ -17,17 +19,26 @@ def get_data():
         subtlety = ann.subtlety
         malignancy = ann.malignancy
         data.append(Vector4D(width, height, subtlety, malignancy))
-        print(f"{count}/{max}: {(count/max)*100}%")
+        print(f"{count}/{max}: {(count / max) * 100}%")
     return data
 
 
 if __name__ == "__main__":
     data = get_data()
-    centroids, clusters, obj = kmeans(data, 5)
-    plt.title("Width & Height. K = 5")
-    plt.xlabel("Width")
-    plt.ylabel("Height")
+    centroids, clusters, obj = kmeans(data, 2)
+    # plt.title("Width & Malignancy. K = 5")
+    # plt.xlabel("Width")
+    # plt.ylabel("Height")
+    # matrix = np.corrcoef(data, data)
+    # pp = pprint.PrettyPrinter(indent=4)
+    # pp.pprint(matrix)
+
     for c in clusters:
         cluster = np.array(c)
-        plt.scatter(cluster[:, 0], cluster[:, 1])
-    plt.show()
+        print(cluster[:, 0].shape)
+        print(cluster[:, 1].shape)
+        print(cluster[:, 2].shape)
+        print(cluster[:, 3].shape)
+
+        plt.scatter(cluster[:, 0], cluster[: 1])
+        plt.show()
